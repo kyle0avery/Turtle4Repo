2c953379b7d760e6e2a9245ed1c4aaffb537ce92
kyle0avery <114882075+kyle0avery@users.noreply.github.com>
2023-11-14 21:06:12 -0500

    Add files via upload

---
+++ #Turtle Goes To Beach.py
@@ -0,0 +1,22 @@
+#Turtle Goes To Beach - Notes
+
+
+'''
+Turtle Leaves House  *input - ask what turtle will collect at the beach, shells, trash, or fish
+Turtle Drives to beach  *nested  loop - dodge obstacle in  traffic
+Turtle picks up trash * Loop -> until trash collected 
+Turtle returns home * 
+
+
+
+functions = 5/3
+main funct to call - duh
+list - present as options for activity at beach
+single loop - picking up trash
+nested loop -  
+if structure (multi alt) - return home?
+input statement
+output to screen -> in honor of FedEx
+
+'''
+
---
+++ Turtle Begins.py
@@ -0,0 +1,55 @@
+'''
+Turtle Begins
+
+
+
+
+
+'''
+
+
+import turtle
+import time
+
+#mainly set up first 
+turtle.setup(900,900)
+turtle.speed = 7
+turtle.goto(0,0)
+
+#drawing turtlehouse
+turtle.pensize(2)
+turtle.pencolor("black")
+turtle.pendown()
+turtle.setheading(0)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(25)
+turtle.right(90)
+turtle.forward(15)
+turtle.left(90)
+turtle.forward(25)
+turtle.right(90)
+turtle.forward(15)
+turtle.setheading(270)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(15)
+turtle.right(90)
+turtle.forward(25)
+turtle.left(90)
+turtle.forward(15)
+turtle.right(90)
+turtle.forward(25)
+turtle.left(90)
+turtle.forward(100)
+turtle.penup()
+turtle.goto(65,-40)
+turtle.pendown()
+turtle.dot(30,"black")
+turtle.penup()
+
+#starting the journey 
+turtle.setheading(90)
+turtle.forward(100)
+#####insert
+turtle.write()
